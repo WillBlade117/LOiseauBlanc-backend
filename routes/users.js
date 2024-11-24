241de4a3aad6f59fb46088e232aba08d8a49b1cf
William <sart.william@gmail.com>
2024-11-24 10:10:28 +0100

    add router signin/signun

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const User = require("../models/users");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
+const { checkBody } = require("../modules/checkBody");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// SIGNUP :
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["firstname", "username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const newUser = new User({
+        firstname: req.body.firstname,
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+        tweets: [],
+      });
+      newUser.save().then((user) => {
+        res.json({ result: true, user: { firstname: user.firstname, username: user.username, token: user.token }});
+      });
+    } else {
+      res.json({ result: false, error: "User already exists" });
+    }
+  });
+});
+
+// SIGNIN :
+router.post("/signin", (req, res, next) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({ username: req.body.username }).then((user) => {
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      res.json({ result: true, user: { firstname: user.firstname, username: user.username, token: user.token } });
+    } else {
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
 });
 
 module.exports = router;
