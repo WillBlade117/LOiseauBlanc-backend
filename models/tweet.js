42a579440c0066681e26d29d713320e65f30b58d
William <sart.william@gmail.com>
2024-11-27 14:30:59 +0100

    models: users/tweets

---
+++ models/tweet.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = mongoose.Schema({
+    date: Date,
+    content: String,
+    hasLiked: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+});
+
+const Tweet = mongoose.model("tweets", tweetSchema);
+
+module.exports = Tweet;
